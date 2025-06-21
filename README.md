84a799005e1bc0312a7f47b017d15fed26c71ff5
Kirey <lit17909@proton.me>
2025-06-20 22:02:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+> ⚠️ This script install browsers using yay/pacman. Make sure you have yay installed.
+
+# ♠️ Arch Browsers Install
+
+A simple CLI tool for installing browsers on Arch Linux.
+It contains around 10 browsers that use Chromium or Gecko.
+The project is still in its infancy, more browsers will be added later.
+
+## 📦 Installation
+
+### 🔹Option 1: One-liner (Recommended)
+
+curl -sSL https://github.com/kirey-arch/arch-browsers-installer/releases/latest/download/install.sh | bash
+
+> Downloads the latest executable file and runs it
+
+### 🔹Option 2: Manual Download
+
+git clone https://github.com/kire-arch/arch-browsers-installer && cd arch-browsers-installer && sudo chmod +x browser-installer.sh && ./browser-installer.sh
+
+## 🔗 Browser Links
+
+In case you're curious or paranoid, here are the official sites:
+
+- [Google Chrome](https://www.google.com/chrome/)
+- [Vivaldi](https://vivaldi.com/)
+- [Brave](https://brave.com/)
+- [Ungoogled Chromium](https://github.com/ungoogled-software/ungoogled-chromium)
+- [Firefox](https://www.mozilla.org/firefox/)
+- [Waterfox](https://www.waterfox.net/)
+- [LibreWolf](https://librewolf.net/)
+- [Tor Browser](https://www.torproject.org/)
+- [Zen Browser](https://aur.archlinux.org/packages/zen-browser-bin) *(AUR link)*
+
+## 🙏 Credits & Acknowledgements
+
+- Script created with a lot of love by [@kirey-arch](https://github.com/kirey-arch).
+
+## ⚠️ Note
+
+This script has no affiliation with any of the projects listed.
+If it breaks, it's Chrome's fault.
---
+++ browser-installer.sh
@@ -0,0 +1,208 @@
+
+confirm_dialog() {
+    echo " ?? Are you sure you want install $1? [Y/N]: "; read confirm
+    confirm=$(echo "$confirm" | tr '[:lower:]' '[:upper:]')
+}
+
+open_chromium_choice() {
+	echo " | ----- Select Chromium based browser to Install ----- | "
+	echo
+	echo " [1] Google Chrome (please don't install this shit)"
+	echo " [2] Vivaldi"
+	echo " [3] Brave"
+	echo " [4] Ungoogled Chromium"
+	echo
+	echo " [0] Go back"
+	echo
+	echo " - Type option number"; read chromium_browser_choice
+
+	case $chromium_browser_choice in
+		"1")
+            confirm_dialog "Google Chrome"
+            if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Google Chrome..."
+				yay -S google-chrome --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_chromium_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+
+		"2")
+            confirm_dialog "Vivaldi"
+
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Vivaldi..."
+				yay -S vivaldi --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_chromium_choice
+			else
+				echo " !! Invalid option."
+			fi	
+			;;
+	
+		"3")
+            confirm_dialog "Brave Browser"
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Brave..."
+				yay -S brave-bin --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_chromium_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+
+		"4")
+            confirm_dialog "Ungoogled Chromium"
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Ungoogled Chromium..."
+				yay -S ungoogled-chromium-bin --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_chromium_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+
+		"0")
+			echo "Ok..."
+			sleep 0.5
+			main_select_base
+			;;
+
+		*)	
+			echo " !! Invalid option."
+			open_chromium_choice
+			;;		
+    esac	
+}
+
+open_gecko_choice() {
+	echo " | ----- Select Gecko based browser to Install ----- | "
+	echo
+	echo " [1] Firefox"
+	echo " [2] Waterfox"
+	echo " [3] LibreWolf"
+	echo " [4] Tor Browser"
+	echo " [5] Zen Browser" # zen-browser-bin
+	echo
+	echo " [0] Go back"
+	echo
+	echo " - Type option number"; read gecko_browser_choice
+
+	case $gecko_browser_choice in
+		"1")
+            confirm_dialog "Firefox"
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Firefox..."
+				yay -S firefox-bin --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_gecko_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+
+		"2")
+            confirm_dialog "Waterfox"
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Waterfox..."
+				yay -S waterfox-bin --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_gecko_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+		
+
+		"3")
+            confirm_dialog "LibreWolf"
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing LibreWolf..."
+				yay -S librewolf-bin --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_gecko_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+
+        "4")
+          confirm_dialog "Tor Browser"
+          if [[ "$confirm" == "Y" ]]; then
+            echo "Installing Tor Browser..."
+            sudo pacman -S torbrowser-launcher --noconfirm
+
+          elif [[ "$confirm" == "N" ]]; then
+            echo "Ok... Exiting."
+            open_gecko_choice
+          else
+            echo " !! Invalid option."
+          fi
+          ;;
+		"5")
+            confirm_dialog "Zen Browser"
+			if [[ "$confirm" == "Y" ]]; then
+				echo "Installing Zen Browser..."
+				yay -S zen-browser-bin --noconfirm
+			elif [[ "$confirm" == "N" ]]; then
+				echo "Ok... Exiting."
+				open_gecko_choice
+			else
+				echo " !! Invalid option."
+			fi
+			;;
+
+
+		"0")
+			echo "Ok..."
+			sleep 0.5
+			main_select_base
+			;;
+
+		*)	
+			echo " !! Invalid option."
+			open_chromium_choice
+			;;			
+    esac
+}
+
+main_select_base() {
+	echo " | ----- Select browser base ----- | "
+	echo
+	echo " [1] Gecko"
+	echo " [2] Chromium"
+	echo
+	echo " [0] Exit"
+	echo
+	echo " - Type option number: "; read choice
+
+	case $choice in
+		"1")
+			open_gecko_choice
+			;;
+		"2")
+			open_chromium_choice
+			;;
+		"0")
+			exit 1
+			;;
+		*)
+			echo " !! Invalid option."
+			main_select_base
+			;;
+	esac
+
+}
+
+main_select_base
